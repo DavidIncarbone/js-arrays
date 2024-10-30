31d66e2c8d456c5a82689491999d96b7b641d071
DavidIncarbone <david.incarbone@gmail.com>
2024-10-30 14:59:59 +0100

    ex-10

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -71,4 +71,5 @@ console.log(teachersString);
 
 // 10. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = false;
+console.log(!teachers.length);
